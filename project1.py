d5d2ccd8f7df3c72b34cd10c946265f808f8f7af
g-rashmi <rashmi.gupta7018@gmail.com>
2024-09-21 20:14:53 +0530

    learning

--- project1.py
+++ project1.py
@@ -3,7 +3,7 @@ import cv2
 #path=input("enter the path of image");
 #Now read image ; r->raw string
 path=r"C:\Users\rashm\OneDrive\Desktop\cp\Learning-OPEN_CV\imgg.png"
-img1= cv2.imread(path,0)
+img1= cv2.imread(path,0) #pass zero for grayscale ;
 img1=cv2.flip(img1,0) #accept 3 parameters 0,-1,1;
 cv2.imshow("converted image",img1)
 k=cv2.waitKey(0);
